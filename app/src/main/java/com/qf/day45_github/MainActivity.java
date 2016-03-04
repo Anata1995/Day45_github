9e501c688bdb973431302ca30c8a357389903e82
anata <Anata1995@sina.com>
2016-03-04 15:45:13 +0800

    提交

--- app/src/main/java/com/qf/day45_github/MainActivity.java
+++ app/src/main/java/com/qf/day45_github/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * 我就是加个注释看看
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
